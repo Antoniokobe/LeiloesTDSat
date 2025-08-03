e58d1567eb3d97d420ae08fcfacf32a6f9dfd4cd
Antonio Carlos <antonioconceicao@outlook.com>
2025-08-03 05:16:15 -0300

    Correção de bug em <listagemView>

--- src/br/com/Leiloes/view/listagemVIEW.java
+++ src/br/com/Leiloes/view/listagemVIEW.java
@@ -212,8 +212,8 @@ public class listagemVIEW extends javax.swing.JFrame {
     }//GEN-LAST:event_btnVenderActionPerformed
 
     private void btnVendasActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVendasActionPerformed
-        listagemVIEW vendas = new listagemVIEW(); 
-        vendas.setVisible(true);
+        
+               
     }//GEN-LAST:event_btnVendasActionPerformed
 
     private void btnVoltarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnVoltarActionPerformed
